101f48768b20d8042154b4a37b93f6a3685ab1ab
Kaloyan Ivanov <kaloyan.ivanov88@gmail.com>
2022-01-22 18:38:26 +0200

    updated

--- Exams/04.12.2017/Instagraph.Data/Configuration.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Instagraph.Data
-{
-    public static class Configuration
-    {
-        public static string ConnectionString = @"Server=.;Database=FastFood;Trusted_Connection=True";
-    }
-}
